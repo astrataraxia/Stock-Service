29799b0b60b7b7e31477ba83ae61d6a39a3113ee
stone <astrataraxia@gmail.com>
2024-08-27 14:50:10 +0900

    Custom Exception

---
+++ customer-service/customer-service/src/main/java/com/sydragon/customerportfolio/exceptions/InsufficientSharesException.java
@@ -0,0 +1,9 @@
+package com.sydragon.customerportfolio.exceptions;
+
+public class InsufficientSharesException extends RuntimeException{
+    private static final String MESSAGE = "Customer [id=%d] does not have enough shares to complete the transaction";
+
+    public InsufficientSharesException(Integer id) {
+        super(MESSAGE.formatted(id));
+    }
+}
