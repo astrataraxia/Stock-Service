197a9e051a139391c36428d238bc2f5eae6b79a3
stone <astrataraxia@gmail.com>
2024-09-06 16:47:48 +0900

    Aggregator-service - Aggregator Service Test code. use mock-server

---
+++ aggregator-service/src/test/java/com/sdragon/aggregatorService/tests/StockPriceStreamTest.java
@@ -0,0 +1,43 @@
+package com.sdragon.aggregatorService.tests;
+
+import com.sdragon.aggregatorService.dto.PriceUpdate;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockserver.model.HttpRequest;
+import org.mockserver.model.HttpResponse;
+import org.mockserver.model.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.test.StepVerifier;
+
+public class StockPriceStreamTest extends AbstractIntegrationTest{
+
+    private static final Logger log = LoggerFactory.getLogger(StockPriceStreamTest.class);
+
+    @Test
+    public void priceStream() {
+        // mock stock-service streaming response
+        var responseBody = this.resourceToString("stock-service/stock-price-stream-200.jsonl");
+        mockServerClient
+                .when(HttpRequest.request("/stock/price-stream"))
+                .respond(
+                        HttpResponse.response(responseBody)
+                                .withStatusCode(200)
+                                .withContentType(MediaType.parse("application/x-ndjson"))
+                );
+
+        // we should get the streaming response via aggregator-service
+        this.client.get()
+                .uri("/stock/price-stream")
+                .accept(org.springframework.http.MediaType.TEXT_EVENT_STREAM)
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .returnResult(PriceUpdate.class)
+                .getResponseBody()
+                .doOnNext(price -> log.info("{}", price))
+                .as(StepVerifier::create)
+                .assertNext(p -> Assertions.assertThat(p.price()).isEqualTo(53))
+                .assertNext(p -> Assertions.assertThat(p.price()).isEqualTo(54))
+                .assertNext(p -> Assertions.assertThat(p.price()).isEqualTo(55))
+                .verifyComplete();
+    }}
